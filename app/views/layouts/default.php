29cfce0ebc1c4221bc9f17f396dc9a30594feb1c
Rafail Zeynalov <rafailzeynalov@hotmail.com>
2020-09-24 09:30:36 +0400

    перед уроком 9

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -25,8 +25,8 @@
 
         <?=$content?>
 
-<!--    --><?//= \\debug(\vendor\core\Db::$countSql); ?>
-<!--    --><?//= \\debug(\vendor\core\Db::$queries) ?>
+<!--    <?//= \\debug(\vendor\core\Db::$countSql); ?>-->
+<!--    <?//= \\debug(\vendor\core\Db::$queries) ?>-->
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="/bootstrap/js/bootstrap.min.js"></script>
